48245ac164a7446d79ff4ba1ef987367ac8d45c0
Jandrei Pitti <jp.pitti@yahoo.com>
2018-02-09 11:19:28 -0700

    add git help menu

---
+++ main.rb
@@ -0,0 +1,20 @@
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+end
+
+def menu
+  puts '1: Enter git command'
+  puts '2: Exit'
+  choice = gets.to_i
+  case choice
+  when 1
+    puts 'Enter git command'
+    puts_git(gets.strip)
+    menu
+  when 2
+    exit
+  else
+    puts 'Invalid Entry'
+    menu
+  end
+end
